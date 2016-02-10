0c2221c8310569f2228c4dfd5e06434bcca96173
Alek97 <petrovskii097@gmail.com>
2016-02-10 20:14:39 +0300

    Добавил AnnouncedModel для получения информации для отправки на сервер

---
+++ ChudoPechka/ChudoPechka/Models/AnnouncedModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ChudoPechkaLib.Data.DataAnnotations;
+using ChudoPechkaLib.Data;
+using ChudoPechkaLib.Models;
+
+namespace ChudoPechka.Models
+{
+    public class AnnouncedModel
+    {
+        public Guid From { get; set; }
+        [ContainLogin]
+        public string To { get; set; }
+        public int Type { get; set; }
+        public static implicit operator  Announced(AnnouncedModel model)
+        {
+            User usr;
+            using (StoreDB db = new StoreDB())
+                usr = db.GetUser(model.To);
+
+            return new Announced
+            {
+                To = usr,
+                From = model.From,
+                Type = model.Type
+            };
+        }
+    }
+}
