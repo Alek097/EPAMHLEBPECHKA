fb101a9383332dc5cbfce1831c0190ed4ca57abb
Alek097 <petrovskii097@gmail.com>
2016-02-10 07:44:29 +0300

    Описал модель данных Announced

--- ChudoPechka/ChudoPechkaLib.Models/Announced.cs
+++ ChudoPechka/ChudoPechkaLib.Models/Announced.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ChudoPechkaLib.Models
 {
-    class Announced
+    public class Announced
     {
+        [Key]
+        public Guid? Id { get; set; }
+        public Guid? UserId { get; set; }
+        public User To { get; set; }
+        public Guid From { get; set; }
+        public int Type { get; set; }
     }
 }
