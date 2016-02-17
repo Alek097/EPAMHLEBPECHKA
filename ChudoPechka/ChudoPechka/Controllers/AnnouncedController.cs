1d816af3b002c2b314b399f3bad9a532ed0f289c
Alek97 <petrovskii097@gmail.com>
2016-02-17 22:59:59 +0300

    добавил дейстрвие Index в AnnouncedController

--- ChudoPechka/ChudoPechka/Controllers/AnnouncedController.cs
+++ ChudoPechka/ChudoPechka/Controllers/AnnouncedController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 using ChudoPechka.Models;
 
@@ -9,6 +10,10 @@ namespace ChudoPechka.Controllers
 {
     public class AnnouncedController : ChudoPechka.Controllers.Base.BaseController
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
         public void SendAnnounced(AnnouncedModel model)
         {
             if (Auth.IsAuthentication)
