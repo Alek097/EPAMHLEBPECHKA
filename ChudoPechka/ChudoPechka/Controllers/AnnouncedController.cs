c25186c9278def82432c0ea75770c17bd315c371
Alek97 <petrovskii097@gmail.com>
2016-02-17 22:53:03 +0300

    Добавил AnnouncedController.cs

---
+++ ChudoPechka/ChudoPechka/Controllers/AnnouncedController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ChudoPechka.Models;
+
+namespace ChudoPechka.Controllers
+{
+    public class AnnouncedController : ChudoPechka.Controllers.Base.BaseController
+    {
+        public void SendAnnounced(AnnouncedModel model)
+        {
+            if (Auth.IsAuthentication)
+                Auth.SendAnnounced(model);
+        }
+    }
+}
