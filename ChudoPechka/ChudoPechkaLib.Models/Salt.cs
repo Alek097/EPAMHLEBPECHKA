f3d6da29060885d49c268d6a87723324db528c69
Alek97 <petrovskii097@gmail.com>
2016-02-20 16:01:21 +0300

    Добавил модель данных Salt.cs

---
+++ ChudoPechka/ChudoPechkaLib.Models/Salt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChudoPechkaLib.Models
+{
+    class Salt
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string SaltString { get; set; }
+    }
+}
