66542e7dae88ff9ee495facb3d1bbbe8f8f30dd7
Alek97 <petrovskii097@gmail.com>
2016-02-25 20:30:10 +0300

    Added OrderController.cs and view Order/Index.cshtml Added action ToOrder in OrderController with Views/Order/ToOrder.cshtml

---
+++ ChudoPechka/ChudoPechka/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChudoPechka.Controllers
+{
+    public class OrderController : ChudoPechka.Controllers.Base.BaseController
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ToOrder(int day)
+        {
+            if (Auth.IsAuthentication)
+            {
+                ViewBag.Day = day;
+                return View();
+            }
+            else
+                return Redirect(Url.Action("LoginIn", "Account"));
+        }
+    }
+}
