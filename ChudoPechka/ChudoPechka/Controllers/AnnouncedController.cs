c0e8fe977cadc8e54db3a5c92809d41342fbd7a3
Alek97 <petrovskii097@gmail.com>
2016-03-10 04:01:38 +0300

    Initial commit: Теперь оповещания подгружаются через ajax Правки-правки-правки в дизайн=)

--- ChudoPechka/ChudoPechka/Controllers/AnnouncedController.cs
+++ ChudoPechka/ChudoPechka/Controllers/AnnouncedController.cs
@@ -13,58 +13,18 @@ namespace ChudoPechka.Controllers
     {
         public ActionResult Index()
         {
+            if (!Auth.IsAuthentication)
+                return Redirect(Url.Action("Index", "Home"));
             return View();
         }
         public ActionResult GetAnnounced()
         {
-            /*
-            ----------ПАМЯТКА---------
-            если page = 50, тогда вернуть первые 50
-            если page = 100, вернуть следующие 50
-            если page = 150, вернуть вторые следующие 50
-            и т.д.
-            */
             if (!Auth.IsAuthentication)
-                return Redirect(Url.Action("Index", "Home"));
+                throw new HttpException(423,"Вы не авторизованны");
             else
             {
                 List<Announced> Anns = Auth.User.Announceds.ToList();
-
-                if (Anns.Count > 50) // Если больше 50 то мы продолжаем, ИНАЧЕ мы возвращаем то что есть
-                {
-                    List<Announced> newAnn = new List<Announced>(50);
-                    int page;
-                    if (string.IsNullOrEmpty(Request.Params["page"]))//ЕСЛИ строка NULL или Empty, page принимает значение по умолчанию. ИНАЧЕ берёт какое кесть 
-                        page = 50;
-                    else
-                        page = int.Parse(Request.Params["page"]);
-
-                    if (page % 50.0 != 0)//Проверка на кратность
-                        throw new ArgumentException("Недопустимое значение, значение должно быть кратно 50", "page");
-                    else
-                    {
-                        int start = 0;
-                        if (Anns.Count < page) //Допустим у нас 130 элементов а запросили 150. Что делать?
-                        {
-                            int min = Anns.Count;
-                            while (min > 0)
-                                min -= 50;
-                            min += 50;
-                            start = Anns.Count - min;
-                            page = Anns.Count;
-                        }
-                        else
-                            start = page - 50;
-
-                        for (; start < page; start++)
-                            newAnn.Add(Anns[start]);
-
-                    }
-
-                    return View(newAnn);
-                }
-                else
-                    return View(Anns);
+                return View(Anns);
             }
         }
         [ValidateAntiForgeryToken]
